dff00c12d2ff3f8c1313797be87d4fe1978d2a85
kdpsc <kleberpaiva@id.uff.br>
2021-08-17 07:48:41 -0300

    Finalizando a lista 2

--- fundamentos-de-programacao/uri-online-judge/1001.py
+++ fundamentos-de-programacao/uri-online-judge/1001.py
@@ -1,3 +1,38 @@
+'''
+https://www.urionlinejudge.com.br/judge/pt/problems/view/1001
+
+Leia 2 valores inteiros  e armazene-os nas variáveis 𝐴 e 𝐵. Efetue a soma de 𝐴
+e  𝐵  atribuindo o seu  resultado na  variável 𝑋. Imprima  𝑋 conforme  exemplo
+apresentado abaixo. Não  apresente  mensagem alguma além daquilo que está sendo
+especificado e não esqueça de imprimir a quebra de linha após o resultado.
+
+Entrada:
+A entrada contém dois valores inteiros.
+
+Saída:
+Imprima o  conteúdo da  variável 𝑋  conforme exemplo  abaixo, com  um espaço em
+branco  antes e  depois da igualdade.  Obs: O X está em maiúsculo e deve ter um
+espaço antes e um espaço depois do sinal de igualdade.
+
+Exemplos:
+
+-------------------------------------------------------------------------------
+Entrada                                     Saída
+-------------------------------------------------------------------------------
+10                                          X = 19
+9
+-------------------------------------------------------------------------------
+Entrada                                     Saída
+-------------------------------------------------------------------------------
+-10                                         X = -6
+4
+-------------------------------------------------------------------------------
+Entrada                                     Saída
+-------------------------------------------------------------------------------
+15                                          X = 8
+-7
+-------------------------------------------------------------------------------
+'''
 a = int(input())
 b = int(input())
 
---
+++ fundamentos-de-programacao/uri-online-judge/1015.py
@@ -0,0 +1,46 @@
+'''
+https://www.urionlinejudge.com.br/judge/pt/problems/view/1015
+
+Leia os quatro valores  correspondentes aos eixos 𝑥 e 𝑦 de dois pontos quaisquer
+no  plano, 𝑝1 = (𝑥1 , 𝑦1) e 𝑝2 = (𝑥2 , 𝑦2) e  calcule  a  distância  entre  eles,
+mostrando 4 casas decimais após a vírgula, segundo a fórmula:
+
+𝑑𝑖𝑠𝑡𝑎𝑛𝑐𝑖𝑎 = √((𝑥2 − 𝑥1)² + (𝑦2 − 𝑦1)²)
+
+Entrada:
+A entrada  contém duas  linhas de  dados. A primeira  linha  contém dois valores
+de ponto  flutuante, 𝑥1 e 𝑦1 , e a segunda  linha  contém outros dois valores de
+ponto flutuante, 𝑥2 e 𝑦2.
+
+Saída
+Calcule e imprima o valor da  distância segundo a fórmula fornecida, com 4 casas
+após o ponto decimal.
+
+Exemplos:
+
+-------------------------------------------------------------------------------
+Entrada                                     Saída
+-------------------------------------------------------------------------------
+1.0 7.0                                     4.4721
+5.0 9.0
+-------------------------------------------------------------------------------
+Entrada                                     Saída
+-------------------------------------------------------------------------------
+-2.5 0.4                                    16.1484
+12.1 7.3
+-------------------------------------------------------------------------------
+Entrada                                     Saída
+-------------------------------------------------------------------------------
+2.5 -0.4                                    16.4575
+-12.2 7.0
+-------------------------------------------------------------------------------
+'''
+
+import math
+
+x1, y1 = map(float, input().split())
+x2, y2 = map(float, input().split())
+
+distancia = math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
+
+print("%0.4f" % distancia)
