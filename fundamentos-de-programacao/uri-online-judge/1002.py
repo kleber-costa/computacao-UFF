710b803f8e88d4f41dbc2d558b8e00254dcfd6f1
kleberdepaiva <kleberdepaiva@outlook.com>
2021-08-20 10:34:17 -0300

    Inserindo comentários

--- fundamentos-de-programacao/uri-online-judge/1002.py
+++ fundamentos-de-programacao/uri-online-judge/1002.py
@@ -1,4 +1,37 @@
-raio = float(input())
-area = raio ** 2 * 3.14159
+'''
+https://www.urionlinejudge.com.br/judge/pt/problems/view/1002
 
-print("A=%1.4f \n" % area)
+A fórmula  para calcular a área de uma circunferência é:area = π ∙ raio 2.
+Considerando para este problema que π = 3,14159. Efetue o cálculo da área,
+elevando o valor de raio ao quadrado e multiplicando por π.
+
+Entrada:
+A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a
+variável raio.
+
+Saída:
+Apresentar a mensagem "A=" seguido  pelo valor da variável area, conforme
+exemplo  abaixo, com 4 casas  após o ponto decimal. Utilize  variáveis de
+dupla precisão (double). Como todos os problemas, não esqueça de imprimir
+o fim de linha após o resultado, caso contrário, você receberá
+"Presentation Error".
+
+Exemplo:
+-------------------------------------------------------------------------------
+Entrada                                     Saída
+-------------------------------------------------------------------------------
+2.0                                         A=12.5664
+-------------------------------------------------------------------------------
+Entrada                                     Saída
+-------------------------------------------------------------------------------
+100.64                                      A=31819.3103
+-------------------------------------------------------------------------------
+Entrada                                     Saída
+-------------------------------------------------------------------------------
+150.0                                       A=70685.7750
+-------------------------------------------------------------------------------
+'''
+
+raio = float(input())
+
+print("A=%1.4f \n" % (raio ** 2 * 3.14159))
--- fundamentos-de-programacao/uri-online-judge/1007.py
+++ fundamentos-de-programacao/uri-online-judge/1007.py
@@ -1,3 +1,42 @@
+'''
+https://www.urionlinejudge.com.br/judge/pt/problems/view/1007
+
+Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a
+diferença do produto de A e B pelo produto de C e D segundo a fórmula:
+DIFERENCA = (A * B - C * D).
+
+Entrada:
+O arquivo de entrada contém 4 valores inteiros.
+
+Saída:
+Imprima a mensagem DIFERENCA com  todas as letras maiúsculas, conforme
+exemplo abaixo, com um espaço em branco antes e depois da igualdade.
+
+Exemplo:
+-------------------------------------------------------------------------------
+Entrada                                     Saída
+-------------------------------------------------------------------------------
+5                                           DIFERENCA = -26
+6
+7
+8
+-------------------------------------------------------------------------------
+Entrada                                     Saída
+-------------------------------------------------------------------------------
+0                                           DIFERENCA = -56
+0
+7
+8
+-------------------------------------------------------------------------------
+Entrada                                     Saída
+-------------------------------------------------------------------------------
+5                                              DIFERENCA = 86
+6
+-7
+8
+-------------------------------------------------------------------------------
+'''
+
 A = int(input())
 B = int(input())
 C = int(input())
