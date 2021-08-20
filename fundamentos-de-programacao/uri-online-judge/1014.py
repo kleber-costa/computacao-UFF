833387332f405371ccb2c7c3ec92bfcc1104a825
kleberdepaiva <kleberdepaiva@outlook.com>
2021-08-20 11:24:29 -0300

    Inserindo comentários

--- fundamentos-de-programacao/uri-online-judge/1014.py
+++ fundamentos-de-programacao/uri-online-judge/1014.py
@@ -1,5 +1,38 @@
+'''
+https://www.urionlinejudge.com.br/judge/pt/problems/view/1014
+
+Calcule  o  consumo médio  de um  automóvel  sendo fornecidos a distância total
+percorrida (em Km) e o total de combustível gasto (em litros).
+
+Entrada:
+O arquivo  de entrada  contém dois valores: um valor  inteiro X representando a
+distância total percorrida (em Km), e um valor real Y  representando o total de
+combustível gasto, com um dígito após o ponto decimal.
+
+Saída:
+Apresente o valor que  representa o consumo médio do automóvel com 3 casas após
+a vírgula, seguido da mensagem "km/l".
+
+Exemplo
+-------------------------------------------------------------------------------
+Entrada                                     Saída
+-------------------------------------------------------------------------------
+500                                         14.286 km/l
+35.0
+-------------------------------------------------------------------------------
+Entrada                                     Saída
+-------------------------------------------------------------------------------
+2254                                        18.119 km/l
+124.4
+-------------------------------------------------------------------------------
+Entrada                                     Saída
+-------------------------------------------------------------------------------
+4554                                        9.802 km/l
+464.6
+-------------------------------------------------------------------------------
+'''
+
 X = int(input())
 Y = float(input())
-media = X / Y
 
-print("%0.3f km/l" % media)
+print("%0.3f km/l" % (X / Y))
--- fundamentos-de-programacao/uri-online-judge/1016.py
+++ fundamentos-de-programacao/uri-online-judge/1016.py
@@ -1,6 +1,38 @@
+'''
+https://www.urionlinejudge.com.br/judge/pt/problems/view/1016
+
+Dois  carros (X e Y) partem  em uma mesma  direção. O carro X sai com velocidade
+constante de 60 Km/h e o carro Y sai com velocidade constante de 90 Km/h.
+
+Em  uma  hora (60 minutos) o carro Y consegue  se distanciar  30  quilômetros do
+carro X, ou seja, consegue se afastar um quilômetro a cada 2 minutos.
+
+Leia a distância (em Km) e calcule quanto tempo leva (em minutos) para o carro Y
+tomar essa distância do outro carro.
+
+Entrada:
+O arquivo de entrada contém um número inteiro.
+
+Saída
+Imprima o tempo necessário seguido da mensagem "minutos".
+
+Exemplo
+-------------------------------------------------------------------------------
+Entrada                                     Saída
+-------------------------------------------------------------------------------
+30                                          60 minutos
+-------------------------------------------------------------------------------
+Entrada                                     Saída
+-------------------------------------------------------------------------------
+110                                         220 minutos
+-------------------------------------------------------------------------------
+Entrada                                     Saída
+-------------------------------------------------------------------------------
+7                                           14 minutos
+-------------------------------------------------------------------------------
+'''
+
 W = int(input())
 X = int(30)
 
-resultado = W / X * 60
-
-print("%d minutos" %resultado)
+print("%d minutos" % (W / X * 60))
